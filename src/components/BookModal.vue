0e6974b6e181624d13e0d240ff17a585f2ec6aa9
Faruk Imamovic <f.imamovic27@gmail.com>
2024-06-17 19:51:23 +0200

    [2m]: added transition to modal

--- src/components/BookModal.vue
+++ src/components/BookModal.vue
@@ -1,55 +1,57 @@
 <template>
-  <div
-    v-if="isOpen"
-    class="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-10 z-50"
-    @click="emit('close-modal')"
-  >
+  <Transition>
     <div
-      class="relative flex flex-col gap-y-4 bg-white max-h-3/4 max-w-screen-md min-h-96 rounded-xl p-4 md:p-12 w-full my-6 mx-2 md:w-2/3"
-      @click="$event.stopPropagation()"
+      v-if="isOpen"
+      class="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-10 z-50"
+      @click="emit('close-modal')"
     >
-      <button
-        @click="emit('close-modal')"
-        class="absolute top-3 right-3 text-gray-500 text-3xl px-2 py-0 bg-transparent hover:text-gray-700 focus:outline-none rounded-lg"
+      <div
+        class="relative flex flex-col gap-y-4 bg-white max-h-3/4 max-w-screen-md min-h-96 rounded-xl p-4 md:p-12 w-full my-6 mx-2 md:w-2/3"
+        @click="$event.stopPropagation()"
       >
-        &times;
-      </button>
-      <h2 class="text-3xl text-blue-500 font-semibold w-11/12">
-        {{ book.title }}
-      </h2>
-      <p>{{ book.description }}</p>
+        <button
+          @click="emit('close-modal')"
+          class="absolute top-3 right-3 text-gray-500 text-3xl px-2 py-0 bg-transparent hover:text-gray-700 focus:outline-none rounded-lg"
+        >
+          &times;
+        </button>
+        <h2 class="text-3xl text-blue-500 font-semibold w-11/12">
+          {{ book.title }}
+        </h2>
+        <p>{{ book.description }}</p>
 
-      <div class="w-full flex justify-between items-center">
-        <h3 class="text-2xl text-blue-500">Comments</h3>
-        <LikeButton
-          :bookId="book.id"
-          :isLiked="isLiked"
-          :likeCount="book.like_count"
-          @like="emit('like', book.id)"
-        />
-      </div>
-      <div class="overflow-y-auto h-56 flex flex-col-reverse">
-        <div
-          v-if="loading === true"
-          class="w-full flex justify-center items-center"
-        >
-          Loading...
+        <div class="w-full flex justify-between items-center">
+          <h3 class="text-2xl text-blue-500">Comments</h3>
+          <LikeButton
+            :bookId="book.id"
+            :isLiked="isLiked"
+            :likeCount="book.like_count"
+            @like="emit('like', book.id)"
+          />
         </div>
-        <div
-          v-else-if="comments.length === 0"
-          class="w-full flex justify-center items-center"
-        >
-          No comments yet :(
+        <div class="overflow-y-auto h-56 flex flex-col-reverse">
+          <div
+            v-if="loading === true"
+            class="w-full flex justify-center items-center"
+          >
+            Loading...
+          </div>
+          <div
+            v-else-if="comments.length === 0"
+            class="w-full flex justify-center items-center"
+          >
+            No comments yet :(
+          </div>
+          <div v-else v-for="comment in comments" :key="comment.id">
+            <Comment :comment="comment" />
+          </div>
         </div>
-        <div v-else v-for="comment in comments" :key="comment.id">
-          <Comment :comment="comment" />
+        <div class="border-t-2 pt-2">
+          <CommentInput @submit-comment="handleCommentSubmit($event)" />
         </div>
-      </div>
-      <div class="border-t-2 pt-2">
-        <CommentInput @submit-comment="handleCommentSubmit($event)" />
       </div>
     </div>
-  </div>
+  </Transition>
 </template>
 
 <script setup>
@@ -105,3 +107,15 @@ watch(isOpen, () => {
   loadComments()
 })
 </script>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
