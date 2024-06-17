667274b6b27b4e6bb3376db34fd735218f841a90
Faruk Imamovic <f.imamovic27@gmail.com>
2024-06-17 20:36:16 +0200

    [5m]: removed pinia from main.js file

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './index.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import VueCookies from 'vue-cookies'
 
 import App from './App.vue'
@@ -9,7 +8,6 @@ import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(VueCookies, { expires: -1 })
 app.use(router)
 
